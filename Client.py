85fb3d36c56d49ff240bde524b37d58d7b2d0b2c
RomainLgvt <ligavant@lacroixrouge-brest.fr>
2018-12-20 12:26:01 +0100

    Creation programme puthon Question 4

--- Client.py
+++ Client.py
@@ -6,13 +6,26 @@ UDP_PORT = 5005
 
 MESSAGE= "cinema"
 
+
+
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
 
+data, addr = sock.recvfrom(1024)
+print ("received message:", data)
 
+idx=0
+test=0
+tab=[]
+for idx in range (4):
+    tab.append(data[idx])
+    
+test= data[3]<<24 | data[2]<<16 | data[1]<<8
 
-while True:
-        data, addr = sock.recvfrom(1024)
-        print ("received message:", data)
+print(tab)
+print(test)
 
-     
+test=str(test)
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+data, addr = sock.recvfrom(1024)
+
